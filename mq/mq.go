9d2892d37f5e5837cb27ae3b8341e0479f163799
zwf <1094646850@qq.com>
2018-12-13 16:42:26 +0800

    add copyright

--- mq/mq.go
+++ mq/mq.go
@@ -1,3 +1,17 @@
+// Copyright 2018 Hurricanezwf. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // mq封装了RabbitMQ的生产者消费者
 // 建议在大流量的情况下生产者和消费者的TCP连接分离,以免影响传输效率, 一个MQ对象对应一个TCP连接
 
